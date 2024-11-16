99a6681f5a5dbc2001f23d6459268530554f69cc
hommp <nursyahit175@gmail.com>
2024-11-16 15:37:00 +0700

    thert

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,34 +1,45 @@
 @extends('master.layout')
 
 @section('content')
-<div class="container" style="padding-top: 100px;">
-    <h1>Daftar Kategori</h1>
-    <a href="{{ route('categories.create') }}" class="btn btn-primary mb-3">Tambah Kategori</a>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($categories as $category)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $category->name }}</td>
-                <td>
-                    <a href="{{ route('categories.edit', $category) }}" class="btn btn-sm btn-warning">Edit</a>
-                    <form action="{{ route('categories.destroy', $category) }}" method="POST" style="display: inline-block;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus kategori ini?')">Hapus</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $categories->links() }}
-</div>
-@endsection
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12" style="padding-top: 100px;">
+
+            <div class="card">
+                <div class="card-header">Categories</div>
+                <div class="m-3 justify-content-between">
+                    <a href="{{ route('categories.create') }}" class="btn btn-primary">Tambah Kategori</a>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($categories as $category)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $category->name }}</td>
+                                <td>
+                                    <a href="{{ route('categories.edit', $category) }}"
+                                        class="btn btn-sm btn-warning">Edit</a>
+                                    <form action="{{ route('categories.destroy', $category) }}" method="POST"
+                                        style="display: inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger"
+                                            onclick="return confirm('Apakah Anda yakin ingin menghapus kategori ini?')">Hapus</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                {{ $categories->links() }}
+            </div>
+        </div>
+    </div>
+    @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,54 +1,64 @@
 @extends('master.layout')
 
 @section('content')
-<div class="container" style="padding-top: 100px;">
-    <h1>Daftar Postingan</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12" style="padding-top: 100px;">
 
-    <!-- Form Pencarian -->
-    <form action="{{ route('posts') }}" method="GET" class="mb-3">
-        <div class="input-group">
-            <input type="text" name="search" class="form-control" placeholder="Cari Judul Postingan..." value="{{ request()->get('search') }}">
-            <button class="btn btn-primary" type="submit">Cari</button>
-        </div>
-    </form>
-
-    <a href="{{ route('posts.create') }}" class="btn btn-primary mb-3">Tambah Postingan</a>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Judul</th>
-                <th>Kategori</th>
-                <th>Gambar</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($posts as $post)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $post->title }}</td>
-                <td>{{ $post->category->name }}</td>
-                <td>
-                    @if ($post->image)
-                    <img src="{{ asset('images/posts/' . $post->image) }}" alt="{{ $post->title }}" class="img-thumbnail" width="100">
-                    @else
-                    -
-                    @endif
-                </td>
-                <td>
-                    <a href="{{ route('posts.show', $post) }}" class="btn btn-sm btn-info">Detail</a>
-                    <a href="{{ route('posts.edit', $post) }}" class="btn btn-sm btn-warning">Edit</a>
-                    <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display: inline-block;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus postingan ini?')">Hapus</button>
+            <div class="card">
+                <div class="card-header">Daftar Postingan</div>
+                <div class="m-3 justify-content-between">
+                    <form action="{{ route('posts') }}" method="GET" class="me-2">
+                        <div class="input-group">
+                            <input type="text" name="search" class="form-control" placeholder="Cari Judul Postingan..."
+                                value="{{ request()->get('search') }}">
+                            <button class="btn btn-primary" type="submit">Cari</button>
+                        </div>
                     </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $posts->links() }}
+                    <a href="{{ route('posts.create') }}" class="btn btn-primary my-3">Tambah Postingan</a>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Judul</th>
+                                <th>Kategori</th>
+                                <th>Gambar</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($posts as $post)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $post->title }}</td>
+                                <td>{{ $post->category->name }}</td>
+                                <td>
+                                    @if ($post->image)
+                                    <img src="{{ asset('images/posts/' . $post->image) }}" alt="{{ $post->title }}"
+                                        class="img-thumbnail" width="100">
+                                    @else
+                                    -
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="{{ route('posts.show', $post) }}" class="btn btn-sm btn-info">Detail</a>
+                                    <a href="{{ route('posts.edit', $post) }}" class="btn btn-sm btn-warning">Edit</a>
+                                    <form action="{{ route('posts.destroy', $post) }}" method="POST"
+                                        style="display: inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger"
+                                            onclick="return confirm('Apakah Anda yakin ingin menghapus postingan ini?')">Hapus</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                {{ $posts->links() }}
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
