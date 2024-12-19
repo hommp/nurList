bff9965eb6dd0f80b75a19ff91932e5b20ffa39c
hommp <nursyahit175@gmail.com>
2024-12-19 20:37:50 +0700

    ref

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,30 +1,27 @@
 @extends('master.layout')
 @section('content')
-    <div class="bg-light">
-        <div class="container" style="margin-top: 100px; margin-bottom: 100px;">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card p-4 shadow">
-                        <h1>Edit Categories</h1>
-                        <form action=" {{ route('categories.update', $category) }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <div class="mb-3">
-                                <label for="name" class="form-label">Name</label>
-                                <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                    id="name" name="name" value="{{ old('name', $category->name) }}">
-                                @error('name')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <button type="submit" class="btn btn-dark">Update</button>
-                                <a class="btn btn-secondary" href="javascript:history.back()">Back</a>
-                            </div>
-                        </form>
-                    </div>
+    <div class="container" style="margin-top: 100px; margin-bottom: 100px;">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card p-4 shadow">
+                    <h1>Edit Categories</h1>
+                    <form action=" {{ route('categories.update', $category) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                                name="name" value="{{ old('name', $category->name) }}">
+                            @error('name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <button type="submit" class="btn btn-dark">Update</button>
+                            <a class="btn btn-secondary" href="javascript:history.back()">Back</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,14 +1,12 @@
 @extends('master.layout')
 @section('content')
-    <div class="bg-light">
-        <div class="container" style="margin-top: 100px; margin-bottom: 100px;">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card p-4 shadow">
-                        <h1>{{ $category->name }}</h1>
-                        <p>Slug: {{ $category->slug }}</p>
-                        <a class="btn btn-dark mb-3" href="javascript:history.back()">Back</a>
-                    </div>
+    <div class="container" style="margin-top: 100px; margin-bottom: 100px;">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card p-4 shadow">
+                    <h1>{{ $category->name }}</h1>
+                    <p>Slug: {{ $category->slug }}</p>
+                    <a class="btn btn-dark mb-3" href="javascript:history.back()">Back</a>
                 </div>
             </div>
         </div>
